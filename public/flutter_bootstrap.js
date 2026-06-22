cf1a8edadcacc48f9edd5dfea066c16aca025b9a
alif180588 <alifmudafza123@gmail.com>
2026-06-22 16:21:05 +0800

    Update frontend build to fix image_picker missing plugin cache issue

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3761153697" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2674061915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
